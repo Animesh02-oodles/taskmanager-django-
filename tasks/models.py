ad9707148fd3f3127de73442741f41b6045e0150
Animesh <animesh.pandey@oodles.io>
2024-12-04 14:40:17 +0530

    first commit

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here. #another another
+# Create your models here.
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)  # Associate tasks with users
